f5fcf143b5e5eead36765f6815cd93cdd02f61b2
arsalaan.naeem <arsalaannaeem@hotmail.com>
2022-11-03 20:01:20 +0000

    Initial commit

---
+++ src/test/java/dev/arsalaan/springbootmongodbrestapi/SpringBootMongodbRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.arsalaan.springbootmongodbrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMongodbRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
